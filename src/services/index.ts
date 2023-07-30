cdc281197833e9c1457c54caaa4a173a5699c5b2
Hjiaxing97 <43169035+hovercao@users.noreply.github.com>
2023-07-30 16:53:28 +0800

    feat: 集成element——plus

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,12 +4,6 @@ import { BASE_URL, TIME_OUT } from '@/services/config';
 const jxRequest = new JxRequest({
   baseURL: BASE_URL,
   timeout: TIME_OUT,
-  interceptors: {
-    requestFulfilledFn: function (config) {
-      console.log('实例请求拦截');
-      return config;
-    },
-  },
 });
 
 export default jxRequest;
--- src/views/Main/index.vue
+++ src/views/Main/index.vue
@@ -1,28 +1,15 @@
 <template>
   <h2>Main</h2>
   <p>{{ countStore.count }}</p>
+  <ElButton>s</ElButton>
 </template>
 
 <script lang="ts" setup>
 import useCountStore from '@/store/module/count.ts';
 import { onMounted } from 'vue';
-import jxRequest from '../../services/index';
+import { ElButton } from 'element-plus';
 
 const countStore = useCountStore();
 
-onMounted(() => {
-  jxRequest
-    .request({
-      url: '/home',
-      interceptors: {
-        requestFulfilledFn: function (config) {
-          console.log('单个请求拦截');
-          return config;
-        },
-      },
-    })
-    .then((res) => {
-      console.log(res);
-    });
-});
+onMounted(() => {});
 </script>
